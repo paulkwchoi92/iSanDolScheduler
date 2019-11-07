dad6c3bb986e757d49677c29f17010d12636c581
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-11-06 22:00:37 -0800

    adding apollo/graphql

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ import BooksScreen from './components/BookScreen';
 import BookDetailScreen from './components/BookDetailScreen';
 import AddBookScreen from './components/AddBookScreen';
 import EditBookScreen from './components/EditBookScreen';
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider } from 'react-apollo'
+
+
 
 const MainNavigator = createStackNavigator({
   Book: { screen: BooksScreen },
@@ -12,6 +18,20 @@ const MainNavigator = createStackNavigator({
   EditBook: { screen: EditBookScreen },
 });
 
-const App = createAppContainer(MainNavigator);
+const MyRootComponent = createAppContainer(MainNavigator); // changing the name of the root component w navigations inside
 
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    url: 'http://localhost:3000/graphql'
+  })
+});
+
+const App = () => {
+  <ApolloProvider client={client}>
+    <MyRootComponent/>
+  </ApolloProvider>
+}
+AppRegistry.registerComponent('MyApp', () => App) // registering apollo provider
 export default App;
